871650ca73a4a9b8441fd7e0a9feba82a96f8fb3
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 00:08:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/empty_mountain_dev_78511/MainActivity.java
@@ -0,0 +1,15 @@
+package com.empty_mountain_dev_78511;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "empty_mountain_dev_78511";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "empty-mountain-dev-78511",
+  url: "https://empty-mountain-dev-78511.botics.co",
+  api: "https://empty-mountain-dev-78511.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
